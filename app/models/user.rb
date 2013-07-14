d8968a23049a264d3157ab286ed3707a8b7738c0
Stephane Bellity <sbellity@gmail.com>
2013-07-14 13:41:27 -0600

    Fix missing name in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ActiveRecord::Base
   LEVELS = ['Beginner', 'Intermediate', 'Advanced']
 
-  attr_accessible :language_id, :language, :level, :zipcode, :firstname, :lastname, :email
+  attr_accessible :language_id, :language, :level, :zipcode, :firstname, :lastname, :email, :name, :login
 
   belongs_to :language
-
-  def self.from_hull_user hull_user_id
-    hull_user = Hull.get(hull_user_id)
-    github_user = hull_user['identities'].select { |i| i['provider'] == 'github' }.first
-    user = User.find_or_create_by_login(github_user['login'])
-    user.update_attributes({ email: hull_user['email'], name: hull_user['name'] })
-    user
-  end
-
+  def self.from_hull_user hull_user_id
+    hull_user = Hull.get(hull_user_id)
+    github_user = hull_user['identities'].select { |i| i['provider'] == 'github' }.first
+    user = User.find_or_create_by_login(github_user['login'])
+    user.update_attributes({ email: hull_user['email'], name: hull_user['name'] })
+    user
+  end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130714192122) do
+ActiveRecord::Schema.define(:version => 20130714193942) do
 
   create_table "languages", :force => true do |t|
     t.string   "language"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130714192122) do
     t.string   "level"
     t.string   "email"
     t.string   "login"
+    t.string   "name"
   end
 
 end
